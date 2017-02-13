8784ee3423e738d7fe623d1d3f5cd4ed9a4f7276
User <User@192.168.2.9>
2017-02-12 19:11:21 -0600

    changes

--- src/mySampleClass.java
+++ src/mySampleClass.java
@@ -1,9 +1,15 @@
+import com.kitchen.mel.ingredients.Ingredient;
 
 public class mySampleClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Howdy folks");
+				// TODO Auto-generated method stub
+		System.out.println("Howdy folks");
+		
+		Ingredient myNewIngredient = new Ingredient();
+		myNewIngredient.setIngredientFullName("Tomato");
+		System.out.println(myNewIngredient.getIngredientFullName());
+
 	}
 
 }
